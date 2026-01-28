0fb170e294ffb056125bc987edc83f824ab0914b
Absolute Value <heejun1708@gmail.com>
2026-01-28 17:09:28 +0900

    feat: solve 7287

---
+++ java/7287/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("4");
+        bw.newLine();
+        bw.write("huijun1708");
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
