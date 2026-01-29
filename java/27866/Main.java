5215e63e14774f6f73baef371d1f7558dace3251
Absolute Value <heejun1708@gmail.com>
2026-01-29 12:32:25 +0900

    feat: solve 27866

---
+++ java/27866/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(s.charAt(i-1)));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
